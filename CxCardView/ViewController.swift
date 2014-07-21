6545430be1237b651e655d0e2d717c720e730569
Zhou Hao <zhouhao@ZhouHaodeMacBook-Pro.local>
2014-07-21 14:41:57 +0800

    Initial Commit

---
+++ CxCardView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CxCardView
+//
+//  Created by ZhouHao on 21/7/14.
+//  Copyright (c) 2014 Zeus Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
